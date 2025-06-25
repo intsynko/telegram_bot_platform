994af7d823d1be808d03137fe2c8f7c7cad762dc
Kostya Intsyn <k.intsyn@bestdoctor.ru>
2025-06-25 16:41:25 +0300

    List of detailed scenario

--- app/apps/scenarios/serializers.py
+++ app/apps/scenarios/serializers.py
@@ -1,8 +1,55 @@
 from rest_framework import serializers
-from apps.scenarios.models import Scenario
+from apps.scenarios.models import Scenario, ScenarioElement
+from apps.forms.models import Form, FormField
+from apps.menu.models import Menu, MenuField
 
 
 class ScenarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scenario
         fields = ['id', 'name']
+
+
+class FormFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormField
+        fields = ['id', 'name', 'field_type', 'hidden', 'default_value', 'required']
+
+
+class FormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Form
+        fields = ['id', 'name', 'final_message', 'fields']
+
+    fields = FormFieldSerializer(many=True)
+
+
+class MenuFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuField
+        fields = ['id', 'name', 'field_type', 'show_condition', 'linked_element']
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'name', 'fields']
+
+    fields = MenuFieldSerializer(many=True)
+
+
+class ScenarioElementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScenarioElement
+        fields = ['id', 'action_type', 'form', 'menu', 'order']
+
+    form = FormSerializer()
+    menu = MenuSerializer()
+
+
+class ScenarioDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Scenario
+        fields = ['id', 'name', 'elements']
+
+    elements = ScenarioElementSerializer(many=True)
--- app/apps/scenarios/views.py
+++ app/apps/scenarios/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
 from .models import Scenario
-from .serializers import ScenarioSerializer
+from .serializers import ScenarioSerializer, ScenarioDetailSerializer
 
 
 class ScenariosViewSet(viewsets.ModelViewSet):
@@ -9,6 +9,14 @@ class ScenariosViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Scenario.objects.filter(owner=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ['retrieve', 'update', 'partial_update']:
+            return ScenarioDetailSerializer
+        elif self.action in ['list', 'delete']:
+            return ScenarioSerializer
+        else:
+            raise NotImplementedError
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
