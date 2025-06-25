5a7b48fe1a0b3a00de69050b631f71d616374594
Kostya Intsyn <k.intsyn@bestdoctor.ru>
2025-06-25 16:30:45 +0300

    Keep session

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './pages/LandingPage';
@@ -8,6 +8,17 @@ import './App.css';
 
 function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/users/auth/user/', {
+      credentials: 'include',
+    })
+      .then(res => res.ok ? res.json() : null)
+      .then(data => {
+        if (data && data.email) setUser(data);
+      });
+  }, []);
+
   return (
     <Router>
       <Header user={user} setUser={setUser} />
